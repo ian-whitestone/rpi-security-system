6ed8eea3787dd9c7aca8c0335500184d07a397e2
Ian Whitestone <ianjameswhitestone@gmail.com>
2016-04-30 22:31:02 +0000

    initial project files

---
+++ measure_distance.py
@@ -0,0 +1,61 @@
+import time
+import RPi.GPIO as GPIO
+
+# Reset GPIO settings
+#GPIO.cleanup()
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGGER, False)
+
+# Allow module to settle
+time.sleep(0.5)
+
+def measure_distance():
+    # Send 10us pulse to trigger
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+    while GPIO.input(GPIO_ECHO)==0:
+      start = time.time()
+
+    while GPIO.input(GPIO_ECHO)==1:
+      stop = time.time()
+
+    # Calculate pulse length
+    elapsed = stop-start
+
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
+
+    # That was the distance there and back so halve the value
+    distance = distance / 2
+    distance_ft=distance*0.0328084
+
+    print ("Distance : %.1f cm" % distance)
+    #print ("Distance : %.1f ft" % distance_ft)
+
+    return
+
+t1 = time.time()
+t2=0
+while (t2-t1)<5:
+    measure_distance()
+    t2=time.time()
+
+
+# Reset GPIO settings
+GPIO.cleanup()
