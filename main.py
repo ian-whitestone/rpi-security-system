db471bbecaf2e5cd723841bf18665c5dcdb33450
Ian Whitestone <ianjameswhitestone@gmail.com>
2016-09-17 22:32:43 +0000

    slack bot, config settings

--- main.py
+++ main.py
@@ -3,27 +3,102 @@ from gpiozero import MotionSensor
 import datetime
 import time
 import subprocess
-import twilio_text as tw
 import os
-import poplib
-from email import parser
-from geopy.distance import vincenty
-from pyicloud import PyiCloudService
-import csv
+import utils
+
+def get_status(bot):
+    user_messages,bot_messages=utils.get_messages(bot,'status')
+    return user_messages[0]['message']
+
+def main():
+    timeout=utils.ConfigSectionMap('timeout') #timeout['loop'] is in minutes
+    timeout=int(timeout['loop'])*60
+    pir = MotionSensor(4)
+    bot=utils.create_bot()
+
+    curr_status=get_status(bot)
+
+    if curr_status=='shutdown':
+        utils.post_message(bot,'status','process shutdown')
+        return
+    else:
+        curr_time=(datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I:%M%p on %B %d, %Y")
+        utils.post_message(bot,'status','process initiated with status: %s @ %s' % (curr_status,curr_time))
+
+    while True:
+        status=get_status(bot)
+
+        ##4 statuses: on,off,reg,shutdown. on/off overide reg. reg enables based on location
+        if status!=curr_status: #detect status changes
+            prev_status=curr_status
+            curr_status=status
+            if curr_status=='shutdown':
+                utils.post_message(bot,'status','process shutdown')
+                break
+            else:
+                utils.post_message(bot,'status','system has been switched from '+prev_status+ ' to '+curr_status)
+            continue
+
+        if curr_status=='on':
+            search(curr_status,pir,bot)
+            continue
+
+        time.sleep(timeout)#wait X min..then check for new statuses
+    return
 
 
+def search(mode,pir,bot):
+    dir_path=os.path.dirname(os.path.realpath(__file__))
+    timeout=utils.ConfigSectionMap('timeout') #timeout['loop'] is in minutes
+    nomotion_timeout=2*int(timeout['nomotion'])
+    timeout=int(timeout['loop'])*60
 
-def motion_detected(curr_time,dir_path):
+    motion_count=0
+    nomotion_count=0
+    motion_data=[]
+    timeout = time.time() + timeout   # X minutes from now
+    while True:
+        if pir.motion_detected:
+            motion_data.append((str((datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I%M%p%d%m%Y")),time.time(),1))
+            nomotion_count=0 ##reset
+            if motion_count==0: ##first time motion is detected
+                curr_time=(datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I%M%p%d%m%Y")
+                motion_detected(curr_time,bot)
+                motion_count+=1
+            else:
+                motion_count+=1
+        else:
+            motion_data.append((str((datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I%M%p%d%m%Y")),time.time(),0))
+            nomotion_count+=1
+
+	    if motion_count>0 and nomotion_count>nomotion_timeout: #motion was previously sensed and has been gone for X seconds..
+                motion_count=0
+                # intruder_gone(curr_time,bot)
+
+        if time.time()>timeout: ##check for status changes
+            status=get_status(bot)
+            if status==mode: #if status has not changed
+                timeout=time.time()+timeout ##reset timeout and continue looping
+            elif status in ['off','shutdown']:
+                utils.write_to_csv(motion_data)
+                break
+        time.sleep(0.5)
+    return
+
+
+def motion_detected(curr_time,bot):
+    dir_path=os.path.dirname(os.path.realpath(__file__))
     print '%s : MOTION DETECTED' % datetime.datetime.now()
     #turn on living room light
-#    subprocess.check_output([dir_path+'/codesend','4478259 -p 0'])
-    #send text
-#    tw.send_text('intruder detected @'+(datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I:%M%p on %B %d, %Y"),'1')
+    #subprocess.check_output([dir_path+'/codesend','4478259 -p 0'])
+    ##send text
+    utils.post_message(bot,'alerts','intruder detected @ '+(datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I:%M%p on %B %d, %Y"))
     ###start recording (max 1 hour...), kill once motion has stopped.
     # subprocess.Popen("raspivid -fps 30 -hf -t 360000 -w 640 -h 480 -o " + curr_time + ".h264", shell=True)
     return
 
-def intruder_gone(curr_time,dir_path):
+def intruder_gone(curr_time,bot):
+    dir_path=os.path.dirname(os.path.realpath(__file__))
     ##kill video
     # subprocess.call(["pkill raspivid"], shell=True)
     ##conver file
@@ -35,120 +110,17 @@ def intruder_gone(curr_time,dir_path):
     return
 
 
-def search(mode):
-    dir_path=os.path.dirname(os.path.realpath(__file__))
-    pir = MotionSensor(4)
-
-    i=0
-    motion_count=0
-    nomotion_count=0
-    motion_data=[]
-    timeout = time.time() + 60*5   # 5 minutes from now
-    while True:
-        if pir.motion_detected:
-            motion_data.append((time.time(),1))
-            nomotion_count=0 ##reset
-            if motion_count==0: ##first time motion is detected
-                curr_time=(datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I%M%p%d%m%Y")
-                motion_detected(curr_time,dir_path)
-                motion_count+=1
-                time.sleep(1)
-            else:
-                motion_count+=1
-        else:
-            motion_data.append((time.time(),0))
-            nomotion_count+=1
-            print nomotion_count
-	    if motion_count>0 and nomotion_count>20: #motion was previously sensed and has been gone for 10 seconds..
-                motion_count=0
-                intruder_gone(curr_time,dir_path)
-        if time.time()>timeout:
-            status=get_emails()
-            if status==mode: #if
-                timeout=time.time()+60*5 ##reset timeout and continue looping
-            elif status in ['off','shutdown']:
-                write_to_csv(motion_data)
-                break
-        time.sleep(0.5)
-    return
-
-def write_to_csv(data):
-    with open('./data/'+str(time.time())+'.csv','w') as out:
-        csv_out=csv.writer(out)
-        csv_out.writerow(['time','motion'])
-        for row in data:
-            csv_out.writerow(row)
-    return
-
-def main():
-    prev_status='on' #default on
-    while True:
-        status=get_emails()
-        ##3 statuses: on,off,reg. on/off overide reg. reg enables based on location
-        if status is not None and status!=prev_status:
-            prev_status=status
-        if prev_status=='on':
-            search(prev_status)
-        elif prev_status=='shutdown':
-            break
-        time.sleep(60) ##600 normally
-
-    return
-
-def get_emails():
-    dir_path=os.path.dirname(os.path.realpath(__file__))
-    f=open(dir_path.split('DoorSensor')[0]+'/mail_auth.txt','rb+')
-    mail_auth=f.read().split()
-
-    pop_conn = poplib.POP3_SSL('pop.gmail.com')
-    pop_conn.user('recent:'+mail_auth[0])
-    pop_conn.pass_(mail_auth[1])
-
-    #Get messages from server:
-    messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
-    # Concat message pieces:
-    messages = ["\n".join(mssg[1]) for mssg in messages]
-    #Parse message into an email object:
-    messages = [parser.Parser().parsestr(mssg) for mssg in messages]
-
-    for message in messages:
-        print message['subject']
-        # print message.keys()
-
-    statuses=[message['subject'] for message in messages if message['subject'] in ['on','off','shutdown']] ##change to regex for things like oN or ON etc.
-
-    if len(statuses)>0:
-        pop_conn.quit()
-        return statuses[-1]
-
-    pop_conn.quit()
-    return None
-
-def location():
-    dir_path=os.path.dirname(os.path.realpath(__file__))
-    f=open(dir_path.split('DoorSensor')[0]+'/loc.txt','rb+')
-    loc=f.read().split()
-
-    # api = PyiCloudService('jappleseed@apple.com', 'password')
-    coords=api.devices['RPcOAtJ84SNlCtdMZYmED+kA6NxEv03Qh5afc0M0X16Ygbfn9OtQsOHYVNSUzmWV'].location()
-    lat=coords['latitude']
-    lon=coords['longitude']
-    me = (round(lat,7), round(lon,7))
-    home=(float(loc[0]),float(loc[1]))
-    print(vincenty(home, me).meters)
-
-    return
-
+print "\n Now running on: \n"
+print (datetime.datetime.now()-datetime.timedelta(hours=4)).strftime("%I%M%p%d%m%Y")
 main()
+
 
 ##TO ADD
 ##1) only turn on/off light if its a certain time of the day
-##2) schedule the system with pcron
-##3) set up dropbox uploader
-##4) try requiing multiple motion detection events in order to trigger text?? (i.e. 5 trips in 30 seconds??)
+##2) set up dropbox uploader & integrate dropbox with slack
+##3) try requiing multiple motion detection events in order to trigger text?? (i.e. 5 trips in 30 seconds??)
 ##5) use phone location to see disable/enable alarm
-##6) hardcoded enable/disable for overriding location...
 ##7) turn this whole thing into a class
-
-##pip install pyicloud
-##pip install geopy
+##9) build in something for dropped internet connections...(if you ever see the error message)
+##10) add a special email to start up system (ie start python script)...need a bash script running to check emails say every 10 min?
+##11) be able to ping system for current status (i.e. is it running?)
