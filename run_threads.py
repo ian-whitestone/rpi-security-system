8d9d8e44d64e0c8abb3352abbab35ad10cc4266f
iwhitest <ianjameswhitestone@gmail.com>
2018-03-31 21:39:26 -0400

    Thread running process for various background jobs

---
+++ run_threads.py
@@ -0,0 +1,23 @@
+"""
+Run the background processes for the home security system
+
+1) Detect who is home
+2) Upload images to S3
+"""
+import threading
+import time
+from app import who_is_home
+from app import s3_upload
+
+def my_func():
+    while True:
+        print ('hello')
+        time.sleep(5)
+
+home_thread = threading.Thread(target=who_is_home.loop, args=())
+home_thread.daemon = True
+home_thread.start()
+
+
+while True:
+    time.sleep(10000)
