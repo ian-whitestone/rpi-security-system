ba78f286263ab61263a2e5ea23d61d5a5e4e570e
ian-whitestone <ianjameswhitestone@gmail.com>
2018-11-03 19:51:00 -0400

    Append to log files

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
+cd /home/pi/rpi-security-system
 nohup gunicorn -c gunicorn.conf run_flask &
-nohup python3 app/who_is_home.py > app/logs/who_is_home.log 2>&1 &
-nohup python3 app/security_system.py > app/logs/security_system.log &
-nohup python3 app/s3_upload.py > app/logs/s3_upload.log &
+nohup python3 app/who_is_home.py >> app/logs/who_is_home.log 2>&1 &
+nohup python3 app/security_system.py >> app/logs/security_system.log &
+nohup python3 app/s3_upload.py >> app/logs/s3_upload.log &
 nohup glances -w -p 52962 --disable-plugin docker --password &
